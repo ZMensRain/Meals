76bdf9f7c93b0d476995662ccf1e4611e0a17184
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-01-21 13:32:42 +0200

    added functions for getting an isar instance and adding recipes

--- lib/helper/isar.dart
+++ lib/helper/isar.dart
@@ -1,1 +1,15 @@
+import 'package:isar/isar.dart';
+import 'package:meal_planner/model/meal.dart';
+import 'package:path_provider/path_provider.dart';
 
+Future<Isar> getIsar() async {
+  var p = await getApplicationDocumentsDirectory();
+
+  return Isar.getInstance() ??
+      await Isar.open([RecipeSchema], directory: p.path);
+}
+
+void addRecipe(Recipe recipe) async {
+  var isar = await getIsar();
+  await isar.writeTxn(() => isar.recipes.put(recipe));
+}
