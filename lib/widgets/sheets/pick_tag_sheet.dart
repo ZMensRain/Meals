9603d844305aa73a6cb0d157e84a11bbaeec107b
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-01-28 10:30:36 +0200

    created a pick tag sheet for picking and creating tags

---
+++ lib/widgets/sheets/pick_tag_sheet.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+import 'package:meal_planner/helper/isar.dart';
+
+class PickTagMenu extends StatefulWidget {
+  const PickTagMenu({super.key, required this.excludeTags});
+  final List<String> excludeTags;
+  @override
+  State<PickTagMenu> createState() => _PickTagMenuState();
+}
+
+class _PickTagMenuState extends State<PickTagMenu> {
+  List<String> tags = [];
+
+  var searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    getRecipeStats().then(
+      (value) {
+        tags = value.tags;
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
+  }
+
+  List<String> getFilteredTags() {
+    var t = tags
+        .where(
+          (element) => element.toLowerCase().trim().contains(
+                searchController.text.toLowerCase().trim(),
+              ),
+        )
+        .toList();
+    t = t.map((e) => e.toLowerCase().trim()).toList();
+
+    var excludedTags =
+        widget.excludeTags.map((e) => e.toLowerCase().trim()).toList();
+
+    for (var excludedTag in excludedTags) {
+      t.remove(excludedTag);
+    }
+
+    return t;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var filteredTags = getFilteredTags();
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SearchBar(
+            hintText: "Tag",
+            controller: searchController,
+            leading: const Icon(Icons.search),
+          ),
+          filteredTags.isEmpty
+              ? Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Sorry we could not find any tags, would you like to create one?",
+                        textAlign: TextAlign.center,
+                        style:
+                            Theme.of(context).textTheme.headlineSmall!.copyWith(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onBackground
+                                      .withOpacity(0.7),
+                                ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          var newTag = await showDialog<String>(
+                            context: context,
+                            builder: (context) {
+                              var newTagValue = "no no code failed";
+
+                              return AlertDialog(
+                                content: TextField(
+                                  decoration:
+                                      const InputDecoration(hintText: "Tag"),
+                                  onSubmitted: (value) {
+                                    if (value.trim() == "") {
+                                      Navigator.pop(context);
+                                    }
+                                    Navigator.pop(context, value.trim());
+                                  },
+                                  onChanged: (value) => newTagValue = value,
+                                ),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () => Navigator.pop(context),
+                                    child: const Text("Cancel"),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      if (newTagValue.trim() == "") {
+                                        Navigator.pop(context);
+                                      }
+
+                                      Navigator.pop(
+                                          context, newTagValue.trim());
+                                    },
+                                    child: const Text("Create"),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                          if (newTag == null) {
+                            return;
+                          }
+                          if (context.mounted) {
+                            Navigator.pop(context, newTag);
+                          }
+                        },
+                        child: const Text("Create"),
+                      ),
+                    ],
+                  ),
+                )
+              : Expanded(
+                  child: ListView.builder(
+                    itemCount: filteredTags.length,
+                    itemBuilder: (context, index) => ElevatedButton(
+                      onPressed: () =>
+                          Navigator.pop(context, filteredTags[index]),
+                      child: Text(
+                        filteredTags[index],
+                      ),
+                    ),
+                  ),
+                ),
+        ],
+      ),
+    );
+  }
+}
