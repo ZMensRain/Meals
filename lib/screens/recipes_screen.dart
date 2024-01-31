a61940b33e75bd7869838db15d1b15a89a034e7e
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-01-31 08:42:01 +0200

    added search to the recipes screen

--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:isar/isar.dart';
 import 'package:meal_planner/helper/isar.dart';
 import 'package:meal_planner/model/meal.dart';
+import 'package:meal_planner/model/recipe_stats.dart';
 import 'package:meal_planner/screens/recipe_screen.dart';
 import 'package:meal_planner/screens/new_recipe_screen.dart';
+import 'package:meal_planner/widgets/filter_widget.dart';
 import 'package:meal_planner/widgets/recipe_card.dart';
 
 class MealsScreen extends StatefulWidget {
@@ -14,79 +15,116 @@ class MealsScreen extends StatefulWidget {
 }
 
 class _MealsScreenState extends State<MealsScreen> {
-  late Stream<void> recipeStream;
-  late Isar isar;
-  bool isUserSearching = false;
-
-  bool isLoadingRecipes = true;
+  late RecipeStats recipeStats;
 
   @override
   void initState() {
     super.initState();
-    getIsar().then(
-      (value) => setState(
-        () {
-          isar = value;
-          isLoadingRecipes = false;
-          recipeStream = value.recipes.watchLazy(fireImmediately: true);
-        },
-      ),
+    getRecipeStats().then(
+      (value) {
+        recipeStats = value;
+        setState(() {
+          minCalories = value.minCalories;
+          maxCalories = value.maxCalories;
+          minMinutes = value.minMinutes;
+          maxMinutes = value.maxMinutes;
+        });
+      },
     );
   }
 
+  String title = "";
+  List<String> includedTags = [];
+  List<String> excludedTags = [];
+  double minCalories = 0.0;
+  double maxCalories = 200.0;
+  int minMinutes = 0;
+  int maxMinutes = 100;
+
   @override
   Widget build(BuildContext context) {
-    if (isLoadingRecipes) {
-      return const Center(child: CircularProgressIndicator.adaptive());
-    }
-
-    return StreamBuilder(
-      stream: recipeStream,
-      builder: (context, snapshot) {
-        List<Recipe> recipes = isar.recipes.where().findAllSync();
-        return Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                IconButton(
-                    onPressed: () {
-                      setState(() {
-                        isUserSearching = !isUserSearching;
-                      });
-                    },
-                    icon: const Icon(Icons.search)),
-                const Spacer(),
-                IconButton(onPressed: () {}, icon: const Icon(Icons.sort)),
-                IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const NewRecipeScreen(),
-                          ));
-                    },
-                    icon: const Icon(Icons.add))
-              ],
-            ),
-            if (isUserSearching) const Placeholder(),
-            Expanded(
-              child: ListView.builder(
-                itemCount: recipes.length,
-                itemBuilder: (context, index) => MealCard(
-                  recipes[index],
-                  onTaped: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => MealScreen(recipes[index]),
-                    ),
-                  ),
-                ),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          FilterWidget(
+            onFilterChanged: (
+              maxCalories,
+              minCalories,
+              maxMinutes,
+              minMinutes,
+              includedTags,
+              excludedTags,
+              searchBarText,
+            ) {
+              setState(() {
+                title = searchBarText;
+                this.includedTags = includedTags;
+                this.excludedTags = excludedTags;
+                this.minCalories = minCalories;
+                this.maxCalories = maxCalories;
+                this.minMinutes = minMinutes;
+                this.maxMinutes = maxMinutes;
+              });
+            },
+            showPlusButton: true,
+            onPlusButton: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const NewRecipeScreen(),
               ),
             ),
-          ],
-        );
-      },
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: getRecipes(
+                title: title,
+                includedTags: includedTags,
+                notIncludedTags: excludedTags,
+                minCalories: minCalories,
+                maxCalories: maxCalories,
+                minMinutes: minMinutes,
+                maxMinutes: maxMinutes,
+              ),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                      child: CircularProgressIndicator.adaptive());
+                }
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(snapshot.error.toString()),
+                  );
+                }
+
+                if (!snapshot.hasData) {
+                  return const Center(
+                    child: Text("Something went wrong"),
+                  );
+                }
+                if (snapshot.data!.isEmpty) {
+                  return const Center(
+                    child: Text("Sorry we couldn't find anything..."),
+                  );
+                }
+
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) => MealCard(
+                    snapshot.data![index],
+                    onTaped: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MealScreen(snapshot.data![index]),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
