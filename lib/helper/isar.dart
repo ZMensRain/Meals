19246366dbf081035b6c9a6569e26ffbd8f1d65f
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-02-02 15:58:18 +0200

    updates recipe stats now on creation of recipe, added function to add a recipe to a weekday and a function to get a recipe for a weekday

--- lib/helper/isar.dart
+++ lib/helper/isar.dart
@@ -1,18 +1,104 @@
 import 'package:isar/isar.dart';
 import 'package:meal_planner/model/meal.dart';
 import 'package:meal_planner/model/recipe_stats.dart';
+import 'package:meal_planner/model/week.dart';
 import 'package:path_provider/path_provider.dart';
 
 Future<Isar> getIsar() async {
   var p = await getApplicationDocumentsDirectory();
 
   return Isar.getInstance() ??
-      await Isar.open([RecipeSchema, RecipeStatsSchema], directory: p.path);
+      await Isar.open([RecipeSchema, RecipeStatsSchema, WeekSchema],
+          directory: p.path);
 }
 
 void addRecipe(Recipe recipe) async {
   var isar = await getIsar();
+  var stats = await getRecipeStats();
+
+  if (stats.maxCalories < recipe.caloriesPerServing) {
+    stats = stats.copyWith(maxCalories: recipe.caloriesPerServing);
+  }
+  if (stats.minCalories > recipe.caloriesPerServing) {
+    stats = stats.copyWith(minCalories: recipe.caloriesPerServing);
+  }
+  if (stats.maxMinutes < recipe.cookTimeInMinutes) {
+    stats = stats.copyWith(maxMinutes: recipe.cookTimeInMinutes);
+  }
+  if (stats.minMinutes > recipe.cookTimeInMinutes) {
+    stats = stats.copyWith(minMinutes: recipe.cookTimeInMinutes);
+  }
+
+  for (var tag in recipe.tags) {
+    if (!stats.tags.contains(tag)) {
+      stats = stats.copyWith(tags: stats.tags + [tag]);
+    }
+  }
+
   await isar.writeTxn(() => isar.recipes.put(recipe));
+  await isar.writeTxn(() => isar.recipeStats.put(stats));
+}
+
+//TODO
+void addTag(Id recipeId) {
+  throw UnimplementedError();
+}
+
+void addRecipeToWeekday(Id recipeId, Weekday day) async {
+  var isar = await getIsar();
+
+  if ((await isar.weeks.get(1)) == null) {
+    isar.writeTxn(() => isar.weeks.put(Week()));
+  }
+
+  Week week = (await isar.weeks.get(1))!;
+
+  switch (day) {
+    case Weekday.monday:
+      week = week.copyWith(monday: (week.monday + [recipeId]).toSet().toList());
+
+      break;
+    case Weekday.tuesday:
+      week =
+          week.copyWith(tuesday: (week.tuesday + [recipeId]).toSet().toList());
+      break;
+    case Weekday.wednesday:
+      week = week.copyWith(
+          wednesday: (week.wednesday + [recipeId]).toSet().toList());
+      break;
+    case Weekday.thursday:
+      week = week.copyWith(
+          thursday: (week.thursday + [recipeId]).toSet().toList());
+      break;
+    case Weekday.friday:
+      week = week.copyWith(friday: (week.friday + [recipeId]).toSet().toList());
+      break;
+    case Weekday.saturday:
+      week = week.copyWith(
+          saturday: (week.saturday + [recipeId]).toSet().toList());
+      break;
+    case Weekday.sunday:
+      week = week.copyWith(sunday: (week.sunday + [recipeId]).toSet().toList());
+      break;
+    default:
+  }
+  isar.writeTxn(() => isar.weeks.put(week));
+}
+
+Future<List<Recipe>> getRecipesForWeekday(Weekday day) async {
+  var isar = await getIsar();
+  var week = isar.weeks.getSync(1);
+
+  var ids = week!.getRecipeIds(day);
+
+  List<Recipe> r = [];
+  isar.recipes.getAllSync(ids).forEach((e) {
+    if (e != null) {
+      r.add(e);
+    }
+  });
+
+  return r;
 }
 
 Future<RecipeStats> getRecipeStats() async {
