abf59d41419caacc1e8e47e1ab4108c304592338
ZMensRain <116457838+ZMensRain@users.noreply.github.com>
2024-02-19 16:06:45 +0200

    fixed list of recipes not updating after changes are made

--- lib/screens/weekday_screen.dart
+++ lib/screens/weekday_screen.dart
@@ -6,9 +6,15 @@ import 'package:meal_planner/screens/week_screen.dart';
 import 'package:meal_planner/widgets/recipe_list.dart';
 import 'package:meal_planner/widgets/sheets/pick_recipe_sheet.dart';
 
-class WeekdayScreen extends StatelessWidget {
+class WeekdayScreen extends StatefulWidget {
   const WeekdayScreen({super.key, required this.weekday});
   final Weekday weekday;
+
+  @override
+  State<WeekdayScreen> createState() => _WeekdayScreenState();
+}
+
+class _WeekdayScreenState extends State<WeekdayScreen> {
   void addRecipeToDay(BuildContext context) {
     showModalBottomSheet(
       backgroundColor: Theme.of(context).colorScheme.background,
@@ -17,8 +23,20 @@ class WeekdayScreen extends StatelessWidget {
       context: context,
       builder: (context) => PickRecipeSheet(
         onPickRecipe: (int id) {
-          addRecipeToWeekday(id, weekday);
+          addRecipeToWeekday(id, widget.weekday);
         },
+      ),
+    );
+  }
+
+  Stream<void> stream = const Stream.empty();
+
+  @override
+  void initState() {
+    super.initState();
+    getIsar().then(
+      (value) => setState(
+        () => stream = value.weeks.watchLazy(),
       ),
     );
   }
@@ -31,7 +49,7 @@ class WeekdayScreen extends StatelessWidget {
         child: const Icon(Icons.add),
       ),
       appBar: AppBar(
-        title: Text(weekday.name.capitalize()),
+        title: Text(widget.weekday.name.capitalize()),
         actions: [
           IconButton(
             onPressed: () => addRecipeToDay(context),
@@ -39,44 +57,49 @@ class WeekdayScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: RecipeList(
-        recipeFuture: getRecipesForWeekday(weekday),
-        onTap: (recipe) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => MealScreen(recipe),
-            ),
-          );
-        },
-        onLongPress: (recipe) {
-          showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: Text(recipe.title),
-              content: Text(
-                "Would you like to remove this recipe from ${weekday.name.capitalize()}?",
-                textAlign: TextAlign.center,
-              ),
-              actions: [
-                TextButton(
-                  style: TextButton.styleFrom(
-                      foregroundColor: Theme.of(context).colorScheme.secondary),
-                  onPressed: () => Navigator.pop(context),
-                  child: const Text("Cancel"),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    removeRecipeFromWeekday(recipe.id, weekday);
-                    Navigator.pop(context);
-                  },
-                  child: const Text("Yes"),
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+      body: StreamBuilder(
+          stream: stream,
+          builder: (context, snapshot) {
+            return RecipeList(
+              recipeFuture: getRecipesForWeekday(widget.weekday),
+              onTap: (recipe) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MealScreen(recipe),
+                  ),
+                );
+              },
+              onLongPress: (recipe) {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text(recipe.title),
+                    content: Text(
+                      "Would you like to remove this recipe from ${widget.weekday.name.capitalize()}?",
+                      textAlign: TextAlign.center,
+                    ),
+                    actions: [
+                      TextButton(
+                        style: TextButton.styleFrom(
+                            foregroundColor:
+                                Theme.of(context).colorScheme.secondary),
+                        onPressed: () => Navigator.pop(context),
+                        child: const Text("Cancel"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          removeRecipeFromWeekday(recipe.id, widget.weekday);
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Yes"),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          }),
     );
   }
 }
